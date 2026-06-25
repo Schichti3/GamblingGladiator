36ae74d40b5c1b41e0bb0e3a48586a70c114b1bb
Jan Schicht <jan.schicht@hegla.de>
2026-06-25 09:31:51 +0200

    fixing ring_bug implementation

--- src/network/include/ring_buf.h
+++ src/network/include/ring_buf.h
@@ -3,29 +3,37 @@
 
 #include <stdint.h>
 #include <assert.h>
+#include <stdbool.h>
 
 //TODO safety checks, nothing is checked yet
 
-#define STANDARD_RING_BUF_IMPL(name, type, size)        \
-  typedef struct name {                                 \
-    type     buf[size];                                 \
-    uint16_t head_idx;                                  \
-    uint16_t tail_idx;                                  \
-  } name;                                               \
-                                                        \
-  static inline void name##_push(name *b, type val) {   \
-    if (b->tail_idx + 1 == b->head_idx) assert(!"ring_buf tail reached head, ring buf overflow"); \
-    b->buf[b->tail_idx] = val;                          \
-    ++b->tail_idx;                                      \
-    if ( b->tail_idx == size - 1 ) b->tail_idx = 0;     \
-  }                                                     \
-                                                        \
-  static inline bool name##_pop(name *b, type *dst) {   \
-    if (b->head_idx == b->tail_idx) return false;       \
-    *dst = b->buf[b->head_idx];                         \
-    ++b->head_idx;                                      \
-    if ( b->head_idx == size - 1 ) b->head_idx = 0;     \
-    return true;                                        \
+#define STANDARD_RING_BUF_IMPL(name, type, size)              \
+  enum { name##_real_size = (size) + 1};                      \
+  typedef struct name {                                       \
+    type     buf[name##_real_size];                           \
+    uint16_t head_idx;                                        \
+    uint16_t tail_idx;                                        \
+  } name;                                                     \
+                                                              \
+  static inline void name##_init(name *b) {                   \
+    b->head_idx = 0;                                          \
+    b->tail_idx = 0;                                          \
+  }                                                           \
+                                                              \
+  static inline void name##_push(name *b, type val) {         \
+    uint16_t next_tail_idx = b->tail_idx + 1;                 \
+    if (next_tail_idx == name##_real_size) next_tail_idx = 0; \
+    if (next_tail_idx == b->head_idx) assert(!"ring_buf tail would reached head, ring buf is full"); \
+    b->buf[b->tail_idx] = val;                                \
+    b->tail_idx = next_tail_idx;                              \
+  }                                                           \
+                                                              \
+  static inline bool name##_pop(name *b, type *dst) {         \
+    if (b->head_idx == b->tail_idx) return false;             \
+    *dst = b->buf[b->head_idx];                               \
+    ++b->head_idx;                                            \
+    if ( b->head_idx == name##_real_size) b->head_idx = 0;    \
+    return true;                                              \
   }
 
 
